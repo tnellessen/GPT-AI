4d10aff601177aa4bdf408ad50015eb43fa57e4b
tnellessen <tnellessen@gmail.com>
2026-01-09 10:42:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,14 +25,12 @@ function Navbar() {
     <div className="bg-black">
       <div className="max-container">
         <nav className="navbar">
-          <a href="https://www.metadata.digital/" className="logo-link">
-            <img className="logo_metalik" src="/image.png" alt="logo" />
-          </a>
+          <img className="logo_metalik" src="/image.png" alt="logo" />
           <ul className={`links-container ul-screen-big nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
-          <li><a className="nav_links_a" href="https://www.metatikdigital.net/">Home</a></li>
-          <li><a className="nav_links_a" href="https://www.metatikdigital.net/about">About</a></li>
-          <li><a className="nav_links_a" href="https://www.metatikdigital.net/services">Services</a></li>
-          <li><a className="nav_links_a" href="https://www.metatikdigital.net/case-studies">Case studies</a></li>
+          <li><a className="nav_links_a" href="/">Home</a></li>
+          <li><a className="nav_links_a" href="/about">About</a></li>
+          <li><a className="nav_links_a" href="/contact">Services</a></li>
+          <li><a className="nav_links_a" href="/casestudies">Case studies</a></li>
           <li><a className="nav_links_a" href="/form">Form</a></li>
           <li><button className="get_in_touch">Get In Touch</button></li>
         </ul>
